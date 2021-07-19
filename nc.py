df5f23254107773c91d46411a7214716f982b97d
Aiden Stockdale <aidenstockdale@gmail.com>
2021-07-19 12:04:12 +0100

    nc connect working

--- nc.py
+++ nc.py
@@ -1,5 +1,5 @@
 """
-A simple python clone of NetCat - a command line networking tool to
+A simple python clone of netcat - a command line networking tool to
 interact with raw sockets.
 
 Intended examples:
@@ -10,20 +10,70 @@ Intended examples:
 
 import argparse
 import socket
+import sys
+import threading
 
-def listen(addr, port, verbosity):
+def fatal_error(err: str):
+    sys.stderr.write("[!] " + err + "\n");
+    sys.exit(-1)
+
+def loop_sending_input_data(s: socket.socket):
+    print("[*] Send data by typing and pressing enter.")
+
+    try:
+        while True:
+            data = input().encode("utf-8")
+            print(f"[>] Sending: {data}")
+            s.send(data)
+    except KeyboardInterrupt:
+        pass
+        
+
+def listen(addr: str, port: int):
     """
-    Sets up a server to listen on (addr, port), and then loops over sending/recieving data.
+    Sets up a server to listen on (addr, port), and then loops over sending/receiving data.
     """
 
     raise NotImplementedError
 
-def connect(addr, port, verbosity):
+def connect(addr: str, port: int):
     """
-    Attempts to connect to (addr, port), and then loops over sending/recieving data.
+    Attempts to connect to (addr, port), and then loops over sending/receiving data.
     """
 
-    raise NotImplementedError
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        s.connect((addr, port))
+
+        print(f"[*] Connected to {addr}:{port}")
+
+        thread = threading.Thread(target=loop_sending_input_data, args=(s,), daemon=True)
+        thread.start()
+
+        while True:
+            data = s.recv(1024)
+
+            if data == b'':
+                break
+
+            print(f"[<] Received: {data}")
+
+            if not thread.is_alive():
+                break
+
+    except ConnectionRefusedError:
+        fatal_error(f"Connection to {addr}:{port} refused.")
+    except ConnectionAbortedError:
+        fatal_error(f"Connection to {addr}:{port} aborted.")
+    except ConnectionResetError:
+        fatal_error(f"Connection to {addr}:{port} reset.")
+    except KeyboardInterrupt:
+        pass
+
+    
+    s.close()
+    print(f"[*] Connection to {addr}:{port} closed.")
 
 def main():
     parser = argparse.ArgumentParser()
@@ -32,13 +82,14 @@ def main():
     parser.add_argument("port", help="The port to interact with", type=int)
 
     parser.add_argument("-l", "--listen", help="Listen on the port-address pair", action="store_true")
-    parser.add_argument("-v", "--verbosity", help="More detailed logging", action="count", default=0)
 
     args = parser.parse_args()
 
+    #TODO: VALIDATE WITH REGEX ON ADDRESS
+
     if args.listen:
-        listen(args.address, args.port, args.verbosity)
+        listen(args.address, args.port)
     else:
-        connect(args.address, args.port, args.verbosity)
+        connect(args.address, args.port)
 
 main()
